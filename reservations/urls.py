828275db815005bd69ea211d46bef9620722444a
MaouanYassir <ysr1180@hotmail.com>
2024-10-23 09:32:58 +0200

    Django Admin configuration

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +5,7 @@ urlpatterns = [
     path('catalogue/', include('catalogue.urls')),
     path('admin/', admin.site.urls),
 ]
+
+admin.site.index_title = "Projet Réservations"
+admin.site.index_header = "Projet Réservations HEADER"
+admin.site.site_title = "Spectacles"
