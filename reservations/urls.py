587b513227cead2bf8edc21534270a0781af297d
MaouanYassir <ysr1180@hotmail.com>
2024-11-04 14:53:44 +0100

    backend authentication

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('catalogue/', include('catalogue.urls')),
+    path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="admin_password_reset",
+    ),
+    path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            extra_context={"site_header": admin.site.site_header}
+        ),
+        name="password_reset_complete",
+    ),
     path('admin/', admin.site.urls),
 ]
-
-
